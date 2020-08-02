bd292aec395158d3eaddb1aeed84faf409983a03
Patrick s m Amorim <patricksmamorim@hotmail.com>
2020-08-02 20:10:22 -0300

    Criacao dos componentes iniciais

--- src/client/sample-project/src/index.js
+++ src/client/sample-project/src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createBrowserHistory } from 'history';
+import { BrowserRouter as Router, Route, Switch, Redirect, matchPath } from 'react-router-dom';
+import Login from './components/Login';
+import Registrar from './components/Registrar';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <Router history={createBrowserHistory} >
+    <Switch>
+      <Route exact path="/" component={Login}></Route>
+      <Route exact path="/Registrar" component={Registrar}></Route>
+    </Switch>
+  </Router>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
